2d77164d2e42502c0719f9d0905a57584ae9f39a
aftabrehan <aftabrehan52422@gmail.com>
2026-05-05 04:21:38 +0500

    feat: protect routes via clerkMiddleware excluding the liveblocks auth endpoint

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,12 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+const isPublicRoute = createRouteMatcher(['/api/liveblocks-auth']);
+
+export default clerkMiddleware(async (auth, req) => {
+  if (!isPublicRoute(req)) {
+    await auth.protect();
+  }
+});
 
 export const config = {
   matcher: [
